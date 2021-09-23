ff84aeda1ca4e2ad8c332b1848b11b007197686d
Edwin DeJesus <edwinstoddart.student@gmail.com>
2021-09-23 17:02:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Library
+            Dictionary<string, int> CharacterCount = new Dictionary<string, int>();
+            string Input;
+
+            // Console Prompt; Ask the user for a string (without numeric characters)
+            Console.WriteLine("Hello, please enter a series of alphabetic characters.\n" +
+                "I'll tell you how many of each character you have entered.\n" +
+                "Please exclude any non-alphabetic characters: ");
+            while (true)
+            {
+                Input = Console.ReadLine();
+                Input = Input.ToLower();
+                if (Utility.CheckCharacters(Input))
+                {
+                    Console.WriteLine("I'm sorry, you've entered an invalid character. Please try again:");
+                    continue;
+                } else
+                {
+                    break;
+                } // End: if()
+            } // End: while()
+
+            Utility.CountCharacters(Input, CharacterCount);
+            Utility.PrintCount(CharacterCount);
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingCharacters
+{
+    class Utility
+    {
+        public static void CountCharacters(string Input, Dictionary<string, int> CharacterCount)
+        {
+            foreach (char c in Input.ToCharArray())
+            {
+                if (CharacterCount.ContainsKey(c.ToString()))
+                {
+                    CharacterCount[c.ToString()]++;
+                }
+                else
+                {
+                    CharacterCount.Add(c.ToString(), 1);
+                } // End: if()
+            } // End: foreach()
+        } // End: CountCharacters()
+
+        public static void PrintCount(Dictionary<string, int> CharacterCount)
+        {
+            foreach (KeyValuePair<string, int> character in CharacterCount)
+            {
+                Console.WriteLine($"{character.Key}: {character.Value}");
+            } // End foreach()
+        } // End: PrintCount()
+
+        public static bool CheckCharacters(string Input)
+        {
+            char[] AllowedChars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'};
+            if (Input.Length == 0) { return true; }
+            foreach (char c in Input)
+            {
+                if (!AllowedChars.Contains(c))
+                {
+                    return true;
+                } // End: if()
+            } // End: foreach()
+            return false;
+        } // End: CheckCharacters()
+    }
+}
